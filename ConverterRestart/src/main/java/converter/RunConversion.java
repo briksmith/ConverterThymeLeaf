d4c17033b40b66355e078e6ae29bd7d11f10c268
Your Name <briksmith@earthlink.net>
2016-09-07 16:01:33 -0500

    Made the page display a result.  Added infrastructure.
    Started with added a Unit class for passing around the units the use
    will enter.
    Will use enumerated types to determine the actual units and class of
    units.
    Haven't hashed out how to do all of this yet.

--- ConverterRestart/src/main/java/converter/RunConversion.java
+++ ConverterRestart/src/main/java/converter/RunConversion.java
@@ -1,17 +1,24 @@
 package converter;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import model.Unit;
 
 @Controller
 public class RunConversion
 {
 
 	@RequestMapping("/runConversion")
-	public String runConversion(ModelMap map){
+	public String runConversion(HttpServletRequest request, ModelMap map){
 		System.out.println("running conversion");
+		System.out.println("printing out put of dropdown units from: " + request.getParameter("listOfUnitsToConvertFrom"));
+		Unit unitsFrom;
+		Unit unitsTo;
+		map.addAttribute("result", 1);
 		return "hello";
 	}
 }
