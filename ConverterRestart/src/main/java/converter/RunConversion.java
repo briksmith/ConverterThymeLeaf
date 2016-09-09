b4fb993a326a6ac4e3be3e38e4970ee77ec9c720
Your Name <briksmith@earthlink.net>
2016-09-09 16:12:02 -0500

    Made constants
    To avoid spelling mismatches, case sensitive bugs, made a bunch of
    constants for getting information off of the request object and what
    values to return and so on.
    
    Started using said constants in the tests as well.

--- ConverterRestart/src/main/java/converter/RunConversion.java
+++ ConverterRestart/src/main/java/converter/RunConversion.java
@@ -1,6 +1,7 @@
 package converter;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,28 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import model.LengthUnits;
 import model.Unit;
 import model.UnitType;
+import utils.Consts;
 
 @Controller
 public class RunConversion
 {
 
-	private static final String HELLO = "hello";
-
 	@RequestMapping("/runConversion")
 	public String runConversion(HttpServletRequest request, ModelMap map){
 		
-		String stringOfNumberOfUnitsToConvertFrom = request.getParameter("numberOfUnitsToConvertFrom");
+		String stringOfNumberOfUnitsToConvertFrom = request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM);
 		if ( stringOfNumberOfUnitsToConvertFrom == null || stringOfNumberOfUnitsToConvertFrom.isEmpty() )  {
-			return HELLO;
+			return Consts.HELLO;
 		}
 		double numberOfUnitsToConvertFrom = Double.parseDouble(stringOfNumberOfUnitsToConvertFrom);
-		String typeOfUnitToConvertFrom = request.getParameter("listOfUnitsToConvertFrom");
-		String typeOfUnitToConvertTo = request.getParameter("listOfUnitsToConvertTo");
+		String typeOfUnitToConvertFrom = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM);
+		String typeOfUnitToConvertTo = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO);
 		
 		Unit unitsFrom = new Unit(numberOfUnitsToConvertFrom,typeOfUnitToConvertFrom, UnitType.length);
 		Unit unitsTo = new Unit(1, typeOfUnitToConvertTo, UnitType.length);
 		double result = LengthUnits.Convert(unitsFrom, unitsTo);
-		map.addAttribute("result", result);
-		return HELLO;
+		map.addAttribute(Consts.RESULT, result);
+		return Consts.HELLO;
 	}
 }
