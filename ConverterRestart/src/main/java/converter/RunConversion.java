36acd62c9ef732deab1e30771132fd77519621df
Your Name <briksmith@earthlink.net>
2016-09-16 17:28:43 -0500

    Not handling all null or empty cases.
    We were assuming we would never have a null units to convert to or from.
    In testing, we got an NPE.  Now, we gracefully handle this case.

--- ConverterRestart/src/main/java/converter/RunConversion.java
+++ ConverterRestart/src/main/java/converter/RunConversion.java
@@ -16,21 +16,41 @@ public class RunConversion
 {
 
 	@RequestMapping("/runConversion")
-	public String runConversion(HttpServletRequest request, ModelMap map){
-		
+	public String runConversion(HttpServletRequest request, ModelMap map)
+	{
+
 		String stringOfNumberOfUnitsToConvertFrom = request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM);
-		if ( stringOfNumberOfUnitsToConvertFrom == null || stringOfNumberOfUnitsToConvertFrom.isEmpty() )  {
+		String typeOfUnitToConvertFrom = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM);
+		String typeOfUnitToConvertTo = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO);
+		boolean validNumberOfUnits = verifyInput(stringOfNumberOfUnitsToConvertFrom);
+		boolean validTypeOfUnitToConvertFrom = verifyInput(typeOfUnitToConvertFrom);
+		boolean validTypeOfUnitToConvertTo = verifyInput(typeOfUnitToConvertTo);
+		if (validNumberOfUnits == false ||
+			validTypeOfUnitToConvertFrom == false ||
+			validTypeOfUnitToConvertTo == false	)
+		{
 			return Consts.HELLO;
 		}
 		double numberOfUnitsToConvertFrom = Double.parseDouble(stringOfNumberOfUnitsToConvertFrom);
-		String typeOfUnitToConvertFrom = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM);
-		String typeOfUnitToConvertTo = request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO);
-		
-		Unit unitsFrom = new Unit(numberOfUnitsToConvertFrom,typeOfUnitToConvertFrom, UnitType.length);
+
+		Unit unitsFrom = new Unit(numberOfUnitsToConvertFrom, typeOfUnitToConvertFrom, UnitType.length);
 		Unit unitsTo = new Unit(1, typeOfUnitToConvertTo, UnitType.length);
 		double result = LengthUnits.Convert(unitsFrom, unitsTo);
 		map.addAttribute(Consts.RESULT, result);
-		map.addAttribute(Consts.LIST_OF_UNITS_TO_CONVERT_FROM ,unitsFrom.getUnitOfMeasure());
+		map.addAttribute(Consts.LIST_OF_UNITS_TO_CONVERT_FROM, unitsFrom.getUnitOfMeasure());
 		return Consts.HELLO;
+	}
+
+	public static boolean verifyInput(String inString)
+	{
+		if (inString == null)
+		{
+			return false;
+		}
+		if (inString.isEmpty())
+		{
+			return false;
+		}
+		return true;
 	}
 }
--- ConverterRestart/src/test/java/Converter/RunConversionTest.java
+++ ConverterRestart/src/test/java/Converter/RunConversionTest.java
@@ -1,6 +1,8 @@
 package Converter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -25,22 +27,35 @@ import utils.Consts;
 public class RunConversionTest
 {
 
-	@Mock HttpServletRequest request;
+	@Mock
+	HttpServletRequest request;
 	RunConversion systemUnderTest;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp()
+	{
 		MockitoAnnotations.initMocks(this);
 		systemUnderTest = new RunConversion();
 	}
-	
+
+	private void verifyNullOrEmptyConditions(String inReturnString, ModelMap inMap)
+	{
+		assertEquals(Consts.HELLO, inReturnString);
+		assertNull(inMap.get(Consts.RESULT));
+		
+	}
+
 	@Test
-	public void testEmptyNumberOfUnits(){
-		Mockito.when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("");
+	public void testEmptyNumberOfUnits()
+	{
+		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn(Consts.FEET);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn(Consts.FEET);
 		ModelMap map = new ModelMap();
-		try{
+		try
+		{
 			String returnString = systemUnderTest.runConversion(request, map);
-			verficiationOfEmptyOfNullUnitsNumberString(returnString);
+			verifyNullOrEmptyConditions(returnString, map);
 		}
 		catch (Exception e)
 		{
@@ -49,20 +64,17 @@ public class RunConversionTest
 		}
 	}
 
-	private void verficiationOfEmptyOfNullUnitsNumberString(String returnString)
-	{
-		assertEquals("hello", returnString);
-		verify(request,  never()).getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM);
-		verify(request, never()).getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO);
-	}
-	
 	@Test
-	public void testNullUnits() {
+	public void testNullNumberOfUnits()
+	{
 		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn(null);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn(Consts.FEET);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn(Consts.FEET);
 		ModelMap map = new ModelMap();
-		try{
+		try
+		{
 			String returnString = systemUnderTest.runConversion(request, map);
-			verficiationOfEmptyOfNullUnitsNumberString(returnString);
+			verifyNullOrEmptyConditions(returnString, map);
 		}
 		catch (Exception e)
 		{
@@ -70,18 +82,118 @@ public class RunConversionTest
 			fail("Should not have gotten here.  No excption should've been thrown.  Did not handle null number of units string correctly.");
 		}
 	}
+
+	@Test
+	public void testNullUnitsToConvertFrom()
+	{
+		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("1");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn(null);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn(Consts.FEET);
+		ModelMap map = new ModelMap();
+		try
+		{
+			String returnString = systemUnderTest.runConversion(request, map);
+			verifyNullOrEmptyConditions(returnString, map);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			fail("Should not have gotten here.  No exception should've been throw.  Did not handle null units to convert from correctly.");
+		}
+	}
 	
-	@Test 
-	public void testAddedUnitsResultToMap(){
+	@Test
+	public void testEmptyUnitsToConvertFrom()
+	{
+		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("1");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn("");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn(Consts.FEET);
+		ModelMap map = new ModelMap();
+		try
+		{
+			String returnString = systemUnderTest.runConversion(request, map);
+			verifyNullOrEmptyConditions(returnString, map);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			fail("Should not have gotten here.  No exception should've been throw.  Did not handle empty units to convert from correctly.");
+		}
+	}
+
+	@Test
+	public void testNullUnitstoConvertTo()
+	{
+		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("1");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn(Consts.FEET);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn(null);
+		ModelMap map = new ModelMap();
+		try
+		{
+			String returnString = systemUnderTest.runConversion(request, map);
+			verifyNullOrEmptyConditions(returnString, map);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			fail("Should not have gotten here.  No exception should've been thrown.  Did not handle null units to convert to correctly.");
+		}
+	}
+	
+	@Test
+	public void testEmptyUnitstoConvertTo()
+	{
+		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("1");
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn(Consts.FEET);
+		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn("");
+		ModelMap map = new ModelMap();
+		try
+		{
+			String returnString = systemUnderTest.runConversion(request, map);
+			verifyNullOrEmptyConditions(returnString, map);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			fail("Should not have gotten here.  No exception should've been thrown.  Did not handle empty units to convert to correctly.");
+		}
+	}
+
+	@Test
+	public void testAddedUnitsResultToMap()
+	{
 		when(request.getParameter(Consts.NUMBER_OF_UNITS_TO_CONVERT_FROM)).thenReturn("3");
 		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_FROM)).thenReturn("Inches");
 		when(request.getParameter(Consts.LIST_OF_UNITS_TO_CONVERT_TO)).thenReturn("Feet");
 		PowerMockito.mockStatic(LengthUnits.class);
-		when(LengthUnits.Convert(any(),any())).thenReturn(.25);
+		when(LengthUnits.Convert(any(), any())).thenReturn(.25);
 		ModelMap map = new ModelMap();
 		String returnString = systemUnderTest.runConversion(request, map);
-		assertEquals("Return string should've been hello and was not.  Was: " + returnString, Consts.HELLO, returnString);
+		assertEquals("Return string should've been hello and was not.  Was: " + returnString, Consts.HELLO,
+				returnString);
 		double result = (double) map.get(Consts.RESULT);
 		assertTrue("Model map should've had .25 as it's answer and did not.  Was: " + result, .25 == result);
 	}
+	
+	@Test
+	public void testVerifyInputEmptyString(){
+		String test = "";
+		boolean output = RunConversion.verifyInput(test);
+		assertFalse("Should return false for empty string and does not.", output);
+	}
+	
+	@Test
+	public void testVerifyNullString() {
+		String test = null;
+		boolean output = RunConversion.verifyInput(test);
+		assertFalse("Should return false for null string and does not.", output);
+	}
+	
+	@Test
+	public void testVerifyGoodString() {
+		String test = "test";
+		boolean output = RunConversion.verifyInput(test);
+		assertTrue("Should return true for non null non empty string and does not.", output);
+	}
+	
 }
